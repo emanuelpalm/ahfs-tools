7cb2d9dbc5c03a5a0393a64fbff6481754e28756
Emanuel Palm <emanuel.palm@gmail.com>
2018-03-27 18:43:15 +0200

    Simplify AHFS comiler Error

--- ahfs/src/compiler/error.rs
+++ ahfs/src/compiler/error.rs
@@ -5,40 +5,27 @@ use super::lexer::Lexeme;
 #[derive(Debug)]
 pub struct Error<'a> {
     kind: ErrorKind,
-    message: Option<Box<str>>,
     lexeme: Lexeme<'a>,
 }
 
 impl<'a> Error<'a> {
     #[inline]
-    pub fn new<M, L>(kind: ErrorKind, message: Option<M>, lexeme: L) -> Self
-        where M: Into<Box<str>>,
-              L: Into<Lexeme<'a>>
+    pub fn new<L>(kind: ErrorKind, lexeme: Lexeme<'a>) -> Self
+        where L: Into<Lexeme<'a>>
     {
-        Error {
-            kind,
-            message: message.map(|m| m.into()),
-            lexeme: lexeme.into()
-        }
+        Error { kind, lexeme: lexeme.into() }
     }
 }
 
 impl<'a> error::Error for Error<'a> {
     fn description(&self) -> &str {
-        if let Some(ref message) = self.message {
-            return message;
-        }
         self.kind.description()
     }
 }
 
 impl<'a> fmt::Display for Error<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let message = match self.message {
-            Some(ref message) => message,
-            None => self.kind.description(),
-        };
-        write!(f, "{}: {}", message, self.lexeme.as_str())
+        write!(f, "{}\n{}", self.kind.description(), self.lexeme)
     }
 }
 
