3a4b2f1d31725c7274aa891243192bfa03c51df1
Emanuel Palm <emanuel.palm@gmail.com>
2018-05-01 19:42:47 +0200

    Add Source::read_{file,files}()

--- ahfs/src/source/mod.rs
+++ ahfs/src/source/mod.rs
@@ -12,7 +12,9 @@ pub use self::lines::Lines;
 pub use self::region::Region;
 pub use self::text::Text;
 
+use std::io;
 use std::ops;
+use std::path::PathBuf;
 use std::result;
 
 /// Refers to a range of bytes within some arbitrary `str`.
@@ -32,6 +34,43 @@ impl Source {
         where S: Into<Box<[Text]>>
     {
         Source { texts: texts.into() }
+    }
+
+    /// Creates new `Source` from file at given `path`.
+    pub fn read_file<P>(path: P) -> io::Result<Source>
+        where P: Into<PathBuf>,
+    {
+        Text::read(path).map(|text| Self::new(vec![text]))
+    }
+
+    /// Reads contents of files at given `paths` into new `Source` instance.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use ahfs::source::Source;
+    /// use std::fs;
+    ///
+    /// let mut paths = fs::read_dir(".").unwrap()
+    ///     .filter_map(|entry| {
+    ///         let entry = entry.unwrap();
+    ///         if !entry.file_type().unwrap().is_file() {
+    ///             return None;
+    ///         }
+    ///         Some(entry.path())
+    ///     });
+    /// let source = Source::read_files(paths).unwrap();
+    /// println!("{:?}", source.texts());
+    /// ```
+    pub fn read_files<I>(paths: I) -> io::Result<Source>
+        where I: IntoIterator,
+              I::Item: Into<PathBuf>,
+    {
+        let mut texts = Vec::new();
+        for path in paths {
+            texts.push(Text::read(path)?);
+        }
+        Ok(Source::new(texts))
     }
 
     /// `Source` texts.
